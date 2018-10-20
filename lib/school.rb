1d86a2ba87185060cda87c12c1155bf42f534107
Grace Choi <oiograceoio@gmail.com>
2018-10-20 04:48:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[:grade]
+      roster[:grade] << name
+    else
+      roster[:grade] = []
+      roster[grade] << name
+    end
+  end
+end
