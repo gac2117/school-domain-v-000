2c6c0632f7017b494625c0ed1fc97580f14b720d
Grace Choi <oiograceoio@gmail.com>
2018-10-20 04:49:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,10 +9,10 @@ class School
 
   def add_student(name, grade)
     if roster[:grade]
-      roster[grade] << name
+      roster[:grade] << name
     else
-      roster[:grade] = []
-      roster[grade] << name
+      roster[grade] = []
+      roster[:grade] << name
     end
   end
 end
